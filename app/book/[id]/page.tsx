f40fdb22ec2b90ecf95e170c8bcb5e940a3a72bd
Blake Lewis <blake@LEWISFAMILY.local>
2026-06-14 16:28:22 -0500

    Update site

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Header from '../../../components/TopHeader';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
@@ -12,18 +13,24 @@ export default function BookDetail() {
     <div className="min-h-screen bg-[#f8f5f0]">
       <Header />
 
-      <div className="flex min-h-[calc(100vh-85px)]">
-        {/* LEFT SIDE - Background Color + Text Overlay */}
-        <div className="flex-1 relative bg-[#1a2a44] hidden lg:block">
-          <div className="absolute inset-0 bg-[url('https://picsum.photos/id/1015/1400/900')] bg-cover opacity-40" />
-          <div className="absolute inset-0 bg-gradient-to-r from-black/70 to-transparent" />
+      <div className="max-w-7xl mx-auto flex min-h-[calc(100vh-85px)]">
+        {/* LEFT - Big Image */}
+        <div className="flex-1 relative hidden lg:block">
+          <Image
+            src="https://picsum.photos/id/1015/1400/900"
+            alt="Toy Maker Factory"
+            fill
+            className="object-cover"
+            priority
+          />
+          <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent" />
         </div>
 
-        {/* RIGHT SIDE - Book Info */}
-        <div className="flex-1 bg-white flex flex-col justify-center p-12 lg:p-16">
+        {/* RIGHT - Book Information */}
+        <div className="flex-1 bg-white p-12 lg:p-20 flex flex-col justify-center">
           <span className="uppercase tracking-widest text-orange-600 font-semibold text-sm">NEW STORY</span>
           
-          <h1 className="text-6xl font-bold mt-4 mb-6 leading-tight text-[#1a2a44]">
+          <h1 className="text-6xl font-bold mt-6 mb-6 leading-tight text-[#1a2a44]">
             TREEHOUSE MYSTERIES
           </h1>
 
