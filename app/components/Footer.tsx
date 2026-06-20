175482c6addc64bc071a40cc2843923484627298
Blake Lewis <blake@LEWISFAMILY.local>
2026-06-19 20:46:30 -0500

    Update Read With Luke

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,62 +4,48 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer className="rwlFooter">
-      <div className="footerLogoBlock">
+      <div className="footerLeft">
+        <img src="/images/luke-thumbs-up.png" alt="Luke" className="footerLuke" />
         <img
           src="/images/read-with-luke-wordmark.png"
           alt="Read With Luke"
           className="footerWordmark"
         />
-
-        <div className="footerLegal">
-          <p>Privacy * Terms</p>
-          <p>ReadwithLuke © 2026</p>
-          <p>Luke's World, LLC. All rights reserved.</p>
-        </div>
       </div>
 
-      <div className="footerLinks">
+      <nav className="footerLinks">
         <Link href="/">Home</Link>
         <Link href="/library">Library</Link>
         <Link href="/learn">Learn with Luke</Link>
         <Link href="/leaderboard">Leaderboard</Link>
+      </nav>
+
+      <div className="footerNewsletter">
+        <form className="footerEmailBar">
+          <input placeholder="email for newsletter" />
+          <button type="submit">
+            <img src="/images/icon-send.png" alt="" />
+          </button>
+        </form>
+
+        <p>
+          Sign up for our newsletter for new books, announcements, games and
+          more!
+        </p>
+      </div>
+
+      <div className="footerLegalLeft">Privacy * Terms</div>
+
+      <div className="footerLegalCenter">
+        ReadwithLuke © 2026
+        <br />
+        Luke’s World, LLC. All rights reserved.
       </div>
 
       <div className="footerSocials">
-        <button>
-          <img src="/images/icon-facebook.png" alt="" />
-        </button>
-
-        <button>
-          <img src="/images/icon-instagram.png" alt="" />
-        </button>
-
-        <button>
-          <img src="/images/icon-youtube.png" alt="" />
-        </button>
-      </div>
-
-      <div className="footerNewsletter">
-        <div className="footerEmailBar">
-          <input
-            placeholder="email for newsletter"
-          />
-
-          <button>
-            <img src="/images/icon-send.png" alt="" />
-          </button>
-        </div>
-
-        <p>
-          Sign up for our newsletter for new books,
-          announcements, games and more!
-        </p>
-
-        <img
-          src="/images/luke-thumbs-up.png"
-          alt="Luke"
-          className="footerLuke"
-        />
+        <button><img src="/images/icon-facebook.png" alt="" /></button>
+        <button><img src="/images/icon-instagram.png" alt="" /></button>
+        <button><img src="/images/icon-youtube.png" alt="" /></button>
       </div>
     </footer>
   );
