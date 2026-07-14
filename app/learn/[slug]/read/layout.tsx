56369e7487d62c9708a4cadfcefa398282be0e22
COLORHACKERS <hello@colorhackers.com>
2026-07-13 19:21:31 -0500

    Delete app/learn/[slug]/read/layout.tsx

--- app/learn/[slug]/read/layout.tsx
+++
@@ -1,9 +0,0 @@
-import LearnGate from "./LearnGate";
-
-export default function LearnReaderLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <LearnGate>{children}</LearnGate>;
-}
